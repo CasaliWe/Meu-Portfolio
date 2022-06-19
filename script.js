d4dea97c40e0a3a5cfd545845e421e4a4b92ded3
Weslei Casali <wesleicasali18@gmail.com>
2022-06-18 23:45:18 -0300

    att carousel

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ setTimeout(() => {
      document.getElementById('startFade').className = 'fadeIn'
 }, 1000);
 
+//Carousel
+document.getElementById('arrow').addEventListener('click', ()=>{
+       var conteudo1 = document.getElementById('conteudo-central')
+       var conteudo2 = document.getElementById('conteudo-central2')
+       if(conteudo1.className == 'd-block'){
+          conteudo1.className = 'd-none'
+          conteudo2.className = 'd-block'
+       } else {
+          conteudo1.className = 'd-block'
+          conteudo2.className = 'd-none'
+       }
+})
+
